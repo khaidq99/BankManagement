294add90b26aca830bb2d084b57ffc568840578a
duongkhai <duongquockhaiptit@gmail.com>
2021-05-28 10:52:41 +0700

    add unit test

---
+++ src/test/java/com/LoanController/processAddLoanTests.java
@@ -0,0 +1,15 @@
+package com.LoanController;
+
+import com.controller.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.ui.Model;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class processAddLoanTests {
+
+
+}
